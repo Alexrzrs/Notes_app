5518b659091e2ddfa3c7ddd5a12e1c5c8dc6e1db
Alexrzrs <98805717+Alexrzrs@users.noreply.github.com>
2023-06-23 10:48:07 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import NavigationTab from "./src/navigation/NavigationTab";
+import "react-native-gesture-handler";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import Navigation from "./src/navigation/Navigation";
+import { UserContext } from "./src/screen/Account";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+    const [loggedUserData, setLoggedUserData] = useState();
+
     return (
-        <NavigationContainer>
-            <NavigationTab />
-        </NavigationContainer>
+        <UserContext.Provider value={{ loggedUserData, setLoggedUserData }}>
+            <NavigationContainer>
+                <Navigation />
+            </NavigationContainer>
+        </UserContext.Provider>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
